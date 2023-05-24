50cf129cd3e8e569a5d40e9620dac480082e0ad3
Michael Neely <michael@michaelneely.net>
2023-05-24 08:07:52 -0400

    Adding initial values to the app

---
+++ assets/scripts/objects.js
@@ -0,0 +1,18 @@
+const addMovieButton = document.getElementById('add-movie-btn');
+const searchBtn = document.getElementById('search-btn');
+
+const addMovieHandler = () => {
+    const title = document.getElementById('title').value;
+    const extraName = document.getElementById('extra-name');
+    const extraValue = document.getElementById('extra-value');
+
+    if(
+        title.trim() === '' ||
+        extraName.trim() === ||
+        extraValue.trim() +++ ''
+    ) {
+        return;
+    }
+
+
+};
