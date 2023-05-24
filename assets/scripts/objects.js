323e9589ec48f8a4f251d0ca80f5ccde8b218d1b
Michael Neely <michael@michaelneely.net>
2023-05-24 09:14:38 -0400

    Mistakenly added renderMovies() function inside addMovieHandler() function

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,30 +1,32 @@
 const addMovieBtn = document.getElementById('add-movie-btn');
 const searchBtn = document.getElementById('search-btn');
 
+const movies = [];
+const renderMovies = () => {
+    const movieList = document.getElementById('movie-list');
+
+    if (movies.length === 0) {
+        movieList.classList.remove('visible');
+        return;
+    } else {
+        movieList.classList.add('visible');
+    }
+    movieList.innerHTML = ''; // clears fields; not ideal but it works for now
+
+    movies.forEach((movie) => {
+        const movieEl = document.createElement('li');
+        movieEl.textContent = movie.info.title;
+        movieList.append(movieEl);
+    });
+};
 const addMovieHandler = () => {
     const title = document.getElementById('title').value;
     const extraName = document.getElementById('extra-name').value;
     const extraValue = document.getElementById('extra-value').value;
 
-    const movies = [];
 
-    const renderMovies = () => {
-        const movieList = document.getElementById('movie-list');
 
-        if (movies.length === 0) {
-            movieList.classList.remove('visible');
-            return;
-        } else {
-            movieList.classList.add('visible');
-        }
-        movieList.innerHTML = ''; // clears fields; not ideal but it works for now
 
-        movies.forEach((movie) => {
-            const movieEl = document.createElement('li');
-            movieEl.textContent = movie.info.title;
-            movieList.append(movieEl);
-        });
-    };
 
     if(
         title.trim() === '' ||
@@ -43,8 +45,6 @@ const addMovieHandler = () => {
     };
     movies.push(newMovie);
     renderMovies();
-
-
 };
 
 addMovieBtn.addEventListener('click', addMovieHandler);
