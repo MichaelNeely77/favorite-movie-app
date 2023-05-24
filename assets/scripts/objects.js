48e9d3358cf573c3cf94f9b5d435b24b9668cf71
Michael Neely <michael@michaelneely.net>
2023-05-24 08:28:31 -0400

    Added the event listener to the 'Add Movies' button and conected the fields to the app

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,18 +1,33 @@
-const addMovieButton = document.getElementById('add-movie-btn');
+const addMovieBtn = document.getElementById('add-movie-btn');
 const searchBtn = document.getElementById('search-btn');
 
 const addMovieHandler = () => {
     const title = document.getElementById('title').value;
-    const extraName = document.getElementById('extra-name');
-    const extraValue = document.getElementById('extra-value');
+    const extraName = document.getElementById('extra-name').value;
+    const extraValue = document.getElementById('extra-value').value;
+
+    const movies = [];
 
     if(
         title.trim() === '' ||
-        extraName.trim() === ||
-        extraValue.trim() +++ ''
+        extraName.trim() === '' ||
+        extraValue.trim() === ''
     ) {
         return;
     }
 
+    const newMovie = {
+        info: {
+            title, // If the key and the value are the same, JavaScript will automatically assign teh value to the key. Will not work, if value is a string
+            [extraName]: extraValue
+        },
+        id: Math.random()
+    };
+    movies.push(newMovie);
+    console.log(newMovie);
+
 
 };
+
+addMovieBtn.addEventListener('click', addMovieHandler);
+
